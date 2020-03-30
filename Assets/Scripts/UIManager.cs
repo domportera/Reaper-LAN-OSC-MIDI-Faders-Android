1a267147752a336ad9c4289aecf66c517a6407e4
Dom Portera <domportera@gmail.com>
2020-03-29 22:16:14 -0400

    options menu humble beginnings

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject optionsPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleOptionsMenu()
+    {
+        optionsPanel.SetActive(!optionsPanel.activeInHierarchy);
+    }
+}
