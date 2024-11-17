2c5267a97254ba322cf905e200c8f383609e88a3
dom <domportera@gmail.com>
2024-11-17 17:04:48 -0500

    begin iconifying UI

--- Assets/Scripts/Colors/ColorSetter.cs
+++ Assets/Scripts/Colors/ColorSetter.cs
@@ -9,12 +9,10 @@ namespace Colors
 		[FormerlySerializedAs("colorType")] [SerializeField]
 		private ColorType _colorType;
 
-		private Image _image;
-		private Text _text;
+		private MaskableGraphic _textOrImage;
 
 		private bool _canColor = true;
-		private bool HasComponent => _image != null || _text != null;
-		private bool _isImage = false;
+		private bool HasComponent => _textOrImage != null;
 
 		private void Awake()
 		{
@@ -36,36 +34,15 @@ namespace Colors
 			GetColoredComponents();
 			if (!_canColor || !HasComponent) return;
 
-			if (!_isImage)
-			{
-				_text.color = colors.GetColor(_colorType);
-			}
-			else
-			{
-				_image.color = colors.GetColor(_colorType);
-			}
+			_textOrImage.color = colors.GetColor(_colorType);
 		}
 
 		private void GetColoredComponents()
 		{
-			if (_text)
+			if (_textOrImage)
 				return;
 			
-			_text = GetComponent<Text>();
-			if (_text == null)
-			{
-				_image = GetComponent<Image>();
-				if (_image == null)
-				{
-					Debug.LogWarning($"{name} has no component to color", this);
-					_canColor = false;
-				}
-				else
-				{
-					_canColor = true;
-					_isImage = true;
-				}
-			}
+			_textOrImage = GetComponent<MaskableGraphic>();
 		}
 	}
 }
