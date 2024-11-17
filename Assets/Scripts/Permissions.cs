dabbae3f3307dc973aa961f44e1bbce82946602d
dom <domportera@gmail.com>
2024-11-17 01:37:11 -0500

    re-add osc

--- Assets/Scripts/Permissions.cs
+++ Assets/Scripts/Permissions.cs
@@ -1,3 +1,6 @@
+#if UNITY_EDITOR
+using UnityEditor.Analytics;
+#endif
 using UnityEngine;
 
 internal static class Permissions
@@ -8,9 +11,13 @@ internal static class Permissions
         
     }
     
-    [RuntimeInitializeOnLoadMethod]
-    private static void SetScreenTimeout()
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    private static void SetEngineVariables()
     {
+        #if UNITY_EDITOR
+        AnalyticsSettings.enabled = false;
+        #endif
+        
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
     }
 }
--- Assets/Scripts/System/OSCSystem.cs
+++ Assets/Scripts/System/OSCSystem.cs
@@ -1,20 +1,35 @@
 using System.Net;
+using OscJack;
 
 namespace System
 {
     public static class OSCSystem
     {
+        private static OscClient _client;
+        private static IPAddress _ip = IPAddress.Parse("127.0.0.1");
+        private static int _port = 9000;
+        
         public static void SetPort(int port)
         {
+            _port = port;
+            RecreateClient();
         }
 
         public static void SetIp(IPAddress address)
         {
-            throw new NotImplementedException();
+            _ip = address;
+            RecreateClient();
+        }
+
+        private static void RecreateClient()
+        {
+            _client?.Dispose();
+            _client = new OscClient(_ip.ToString(), _port);
         }
 
         public static void Send(string address, float valueToSend)
         {
+            _client.Send(address, valueToSend);
         }
     }
 }
